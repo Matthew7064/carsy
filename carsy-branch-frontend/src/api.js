e7aa6df64ff88a4ac392c015ae51f0fc714dc7bf
PeterSheeper <petersheeper@outlook.com>
2025-06-18 15:44:00 +0200

    Quick url fix

--- carsy-branch-frontend/src/api.js
+++ carsy-branch-frontend/src/api.js
@@ -1,7 +1,7 @@
 import { store } from './store';
 import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'http://localhost:8081';
 
 function convertCarToFrontend(car) {
   return {
